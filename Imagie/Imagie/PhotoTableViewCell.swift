1281e93dc2ab41e4caa963e3eaa84ba0e49a41ca
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-05-13 22:13:13 -0400

    create table view cell file and set the custom cell to it

---
+++ Imagie/Imagie/PhotoTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoTableViewCell.swift
+//  Imagie
+//
+//  Created by Julian A. Fordyce on 5/13/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+ 
+
+}
