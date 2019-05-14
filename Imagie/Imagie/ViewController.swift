26b6f57fe748afcb8e6fdf9c067b246059f2412b
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-05-13 22:03:17 -0400

    add starter Xcode project

---
+++ Imagie/Imagie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Imagie
+//
+//  Created by Julian A. Fordyce on 5/13/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
